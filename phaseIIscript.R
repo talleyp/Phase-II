af0689359b99db5ee2a8c1ca18222e51debb23c5
talleyp <talleyp93@gmail.com>
2015-03-23 14:49:14 -0400

    Added PNG element
    Allows plots to be returned

--- phaseIIscript.R
+++ phaseIIscript.R
@@ -18,14 +18,15 @@ getLinReg <- dbGetQuery(conn, "QUERY")
 getProductReg <- dbGetQuery(conn, "QUERY")
 
 
-histmakerRetail(getRetailBar, "INSERT RECEIPT COST")
-histmakerRegion(getRegionBar, "INSERT RECEIPT COST")
+histmakerRetail(getRetailBar, "INSERT retailer ID")
+histmakerRegion(getRegionBar, "INSERT region ID")
 plotmakerMonth(getLinRed)
 productplot(getProductReg)
 
 #Receives a table with 2 columns, retailer and amount
 #Creates a bar plot for revenue by Retailer ID
-histmakerRetail <- function(directory, x){        
+histmakerRetail <- function(directory, x){  
+        png(filename="barRetail.png", width=500, height=500, type="cairo")
         aggdata = aggregate(directory, by=list(x), FUN=sum)
         aggfix = cbind(aggdata[1], aggdata[3])
         rowid = aggfix[,1]
@@ -38,13 +39,18 @@ histmakerRetail <- function(directory, x){
                 xlab = colnames(aggfix[1]),
                 ylab = colnames(aggfix[2]),
                 names.arg = rowid)
-                
+        dev.off()        
 }
 
 #Receives a table with 2 columns, region and amount
 #Creates a bar plot for revenue by Region
-histmakerRegion <- function(directory, x){        
+histmakerRegion <- function(directory, x){   
+        png(filename="barRegion.png", width=500, height=500, type="cairo")
+        
+        #combines the amount based on retailer ID
         aggdata = aggregate(directory, by=list(x), FUN=sum)
+        
+        #Takes away unnecssary column
         aggfix = cbind(aggdata[1], aggdata[3])
         rowid = aggfix[,1]
         rev = aggfix[,2]
@@ -56,11 +62,13 @@ histmakerRegion <- function(directory, x){
                 xlab = colnames(aggfix[1]),
                 ylab = colnames(aggfix[2]),
                 names.arg = rowid)
-        
+        dev.off()
 }
 
 #Creates a plot for revenue vs time
 plotmaker <- function(directory){
+        png(filename="predRev.png", width=500, height=500, type="cairo")
+        
         #splits the data by date
         short.date = strftime(directory$date, "%Y/%m")
         #create revenue by date
@@ -92,15 +100,16 @@ plotmaker <- function(directory){
                sd(y) / sd(x),  #slope
                lwd = 2)
         points(mean(x), mean(y), cex = 2, pch = 19) #big point of intersection
+        dev.off()
 }
 
 #Creates a plot for product vs time
 productplot <- function(directory){
+        png(filename="predProd.png", width=500, height=500, type="cairo")
+        
         #splits the data by date
         short.date = strftime(directory$date, "%Y/%m")
 
-        
-        
         #plot the original data points with larger dots for more freq pts
         y <- short.date$productID
         x <- short.date
@@ -126,4 +135,5 @@ productplot <- function(directory){
                sd(y) / sd(x),  #slope
                lwd = 2)
         points(mean(x), mean(y), cex = 2, pch = 19) #big point of intersection
+        dev.off()
 }
